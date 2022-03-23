f979492764e960dfec48ddf1a0c06a7e025f2fd8
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-03-23 15:18:12 -0700

    created function to parse all words json file

---
+++ WordleSwift/Reader.swift
@@ -0,0 +1,22 @@
+//
+//  Reader.swift
+//  WordleSwift
+//
+//  Created by Neal Archival on 3/23/22.
+//
+
+import Foundation
+
+func InitWordsArray() -> Array<String> {
+    var wordsArray: Array<String>
+    let filePath = Bundle.main.path(forResource: "words-five-len", ofType: ".json")
+    let urlPath = URL(fileURLWithPath: filePath!)
+    do {
+        let json_data = try Data(contentsOf: urlPath)
+        wordsArray = try JSONDecoder().decode(Array<String>.self, from: json_data)
+    } catch {
+        print("There was an error trying to read file")
+        return []
+    }
+    return wordsArray // successful run
+}
