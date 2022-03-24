819c76625a2ed4fc60863b74849e76d36d44d831
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-03-23 17:43:54 -0700

    keyboard not deletes characters and does not update last char of board when full

--- WordleSwift/KeyboardButton.swift
+++ WordleSwift/KeyboardButton.swift
@@ -24,8 +24,12 @@ struct KeyboardButton: View {
         }
     }
     func KeyButtonPress() {
-        inputSlot = keyCharacter;
-        moveNextCol()
+        // if we are at the last index (we don't exceed past array.count - 1), check if the current slot is empty
+        // if it's not empty, then we don't update slot, otherwise, update
+        if (inputSlot == "") {
+            inputSlot = keyCharacter;
+        }
+        moveNextCol() // columnNum never exceeds array.count - 1
     }
 }
 
