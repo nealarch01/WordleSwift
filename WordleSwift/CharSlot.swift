aadd7bba24728fec25f422c285d3d64ba4decb64
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-04-12 21:51:59 -0700

    Added animations to grids; when a letter is entered, an animation will occur

--- WordleSwift/CharSlot.swift
+++ WordleSwift/CharSlot.swift
@@ -10,15 +10,30 @@ import SwiftUI
 struct CharSlot: View {
     var currentChar: String
     @Binding var slotColor: Color
+    @State private var isAnimated: Bool = false
     var body: some View {
         HStack {
             Spacer()
             Text(currentChar == "" ? " " : currentChar)
             // purpose of putting an empty space to apply max padding (when there is not text in a box, the padding of each box causes uneven spacing
-                .background(Rectangle()
-                    .fill(slotColor.opacity(0.9))
-                    .frame(width: 70, height: 70)
-                    .border(Color.gray, width: 3)
+                .background(
+                    Rectangle()
+                        .fill(slotColor.opacity(0.1))
+                        .frame(width: 70, height: 70)
+                        .border(Color.gray, width: 3)
+                        .scaleEffect(isAnimated ? 1.2 : 1.0)
+                        .onChange(of: currentChar) {_ in
+                            // conditional statement to check if currentCharacter is not empty,
+                            // only show animation when typing, not when deleting
+                            // note: the value of currentChar will not update until AFTER .onChange instance method completes
+                            if currentChar != "" {
+                                return // return to not display an animation
+                            }
+                            isAnimated = true
+                            withAnimation(.easeOut(duration: 0.3)) {
+                                isAnimated = false
+                            }
+                        }
                 )
                 .font(.system(size: 36, weight: .heavy))
                 .foregroundColor(Color.white)
